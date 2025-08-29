9fff351e61285455370caaa64ab1c661513b75c7
shubham <gagareshubham0@gmail.com>
2025-08-29 21:21:11 +0530

    Add project files.

---
+++ EMovies_Ticket/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EMovies_Ticket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
